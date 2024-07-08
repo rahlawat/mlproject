13e60585d53338afe2023772fbfc77c2d19ef228
Renu Ahlawat <renu.ahlawat21@gmail.com>
2024-07-08 14:31:03 +0530

    Adde hyperparam tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -49,7 +49,37 @@ class ModelTrainer():
                 "AdaBoost Classifier": AdaBoostClassifier(),
             }
 
-            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test = X_test, y_test=y_test,models=models)
+            params = {
+                "Random Forest": {
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
+                },
+                "Gradient Boosting": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample': [0.6,0.7,0.75,0.8,0.85,0.9],
+                    #'n_estimator': [8,16,32,64,128,256]
+                },
+                "Linear Regression": {},
+                "K-Neighbors Classifier": {
+                    'n_neighbors': [5,7,9,11]
+                },
+                "XGBClassifier": {
+                    'learning_rate': [.1,0.01,0.05,0.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "AdaBoost Classifier": {
+                    'learning_rate': [.1,.01,0.5,0.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+            model_report:dict=evaluate_models(X_train=X_train,
+                                            y_train=y_train,
+                                            X_test = X_test,
+                                            y_test=y_test,
+                                            models=models,
+                                            params= params)
 
             best_model_score = max(sorted(model_report.values()))
 
